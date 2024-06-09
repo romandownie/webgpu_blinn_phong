e9ec74c8329187bb260add5cb7230f0c059990a1
Roman Downie <romandownie@gmail.com>
2024-06-09 01:23:34 -0600

    added blinn phong with some hard coded values

--- frag.js
+++ frag.js
@@ -1,7 +1,38 @@
 let frag_wgsl = `
+@group(0) @binding(0) var<uniform> vp: mat4x4f; 
+
+
+
 @fragment
-fn main() -> @location(0) vec4f {
-  return vec4(1.0, 0.0, 0.0, 1.0);
+fn main(@location(0) fragPos: vec4<f32>) -> @location(0) vec4f {
+
+  let testLight = vec4f(0.0, 5.0, 5.0, 1.0);
+  let testCameraPos = vec4f(0.0, 20.0, -20.0, 1.0);
+
+  let normal = normalize(vec3f(0.0, 1.0, 0.0)); //normal hard coded as straight up right now
+
+  //blinn phong lighting model, reference: https://learnopengl.com/Advanced-Lighting/Advanced-Lighting
+  let lightDirVec = testLight.xyz - fragPos.xyz;
+  let lightDirN = normalize(lightDirVec);
+  let viewDir = normalize(testCameraPos.xyz - fragPos.xyz);
+  let halfwayDir = normalize(lightDirN + viewDir);
+
+  let distanceSqr = pow(length(lightDirVec), 2.0);
+  let intensity = max(dot(normal, lightDirN), 0.0);
+  let diffuse = vec3f(0.9, 0.1, 0.1) * intensity * (20/distanceSqr);
+
+  let spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0); // 32.0 is shininess here
+  let specular = vec3f(1.0,1.0,1.0) * spec; //hardcoded white
+
+  let ambient = vec4f(0.3, 0.03, 0.03, 1.0);
+
+  
+  //let finalColor = vec4f(specular, 1.0); // just specular for testing
+  //let finalColor = vec4f(diffuse, 1.0); // just diffuse for testing
+  //let finalColor = ambient; // just ambient for testing
+  let finalColor = vec4f(diffuse, 1.0) + vec4f(specular, 1.0) + ambient; // diffuse + specular + ambient
+
+  return finalColor;
 }
 `;
 export default frag_wgsl;
--- vert.js
+++ vert.js
@@ -1,13 +1,20 @@
 let vertex_wgsl = `
 @group(0) @binding(0) var<uniform> vp: mat4x4f; 
 
+struct VertexOutput {
+  @builtin(position) Position : vec4f,
+  @location(0) fragPos: vec4f,
+}
+
 @vertex
 fn main(
   @location(0) inPosition: vec4f,
   @builtin(vertex_index) VertexIndex: u32
-) -> @builtin(position) vec4f {
-  let test = vp;
-  return vp * inPosition;
+) -> VertexOutput {
+  var output : VertexOutput;
+  output.Position = vp * inPosition;
+  output.fragPos = inPosition;
+  return output;
 }
 `;
 export default vertex_wgsl;
